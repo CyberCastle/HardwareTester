497dc89324044e9be6e25d02c2e7ad82c478918a
CyberCastle <cybercastle@gmail.com>
2019-09-19 17:37:08 -0300

    Add hammer.js dependency

--- src/app/core/angular-material.module.ts
+++ src/app/core/angular-material.module.ts
@@ -32,6 +32,8 @@ import { MatTableModule } from '@angular/material/table'
 import { MatSortModule } from '@angular/material/sort'
 import { MatPaginatorModule } from '@angular/material/paginator'
 
+import 'hammerjs'
+
 @NgModule({
     declarations: [],
     imports: [
