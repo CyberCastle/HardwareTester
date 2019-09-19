11f02f437e8ffb82a3cc6da3eba29e003c4e197c
CyberCastle <cybercastle@gmail.com>
2019-09-18 23:02:03 -0300

    Enable Single Instance Lock, adjust windows size, add more items to menu, add others adjustements

--- main.ts
+++ main.ts
@@ -2,23 +2,35 @@ import { app, BrowserWindow, screen, Menu } from 'electron'
 import * as path from 'path'
 import * as url from 'url'
 
-let win: BrowserWindow, serve: boolean
+let mainWindow: BrowserWindow
 const args = process.argv.slice(1)
-serve = args.some(val => val === '--serve')
+const serve: boolean = args.some(val => val === '--serve')
+const dev: boolean = args.some(val => val === '--dev')
 
 function createWindow() {
     const electronScreen = screen
     const size = electronScreen.getPrimaryDisplay().workAreaSize
 
     // Create the browser window.
-    win = new BrowserWindow({
-        x: 0,
-        y: 0,
-        width: size.width,
-        height: size.height,
+    mainWindow = new BrowserWindow({
+        title: 'HardwareTester',
+        //x: 0,
+        //y: 0,
+        center: true,
+        width: size.width - 50,
+        height: size.height - 50,
+        resizable: false,
+        fullscreenable: false,
+        transparent: true,
+        // Remove the window frame from windows applications
+        frame: false,
+        // Hide the titlebar from MacOS applications while keeping the stop lights
+        titleBarStyle: 'hiddenInset', // or 'customButtonsOnHover',
         webPreferences: {
             nodeIntegration: true,
+            nodeIntegrationInWorker: true,
             backgroundThrottling: false,
+            disableHtmlFullscreenWindowResize: true,
         },
     })
 
@@ -26,9 +38,9 @@ function createWindow() {
         require('electron-reload')(__dirname, {
             electron: require(`${__dirname}/node_modules/electron`),
         })
-        win.loadURL('http://localhost:4200')
+        mainWindow.loadURL('http://localhost:4200')
     } else {
-        win.loadURL(
+        mainWindow.loadURL(
             url.format({
                 pathname: path.join(__dirname, 'dist/index.html'),
                 protocol: 'file:',
@@ -38,72 +50,119 @@ function createWindow() {
     }
 
     // Open the DevTools.
-    win.webContents.openDevTools()
+    if (dev) {
+        mainWindow.webContents.openDevTools()
+    }
 
     // Emitted when the window is closed.
-    win.on('closed', () => {
+    mainWindow.on('closed', () => {
         // Dereference the window object, usually you would store window
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
-        win = null
+        mainWindow = null
     })
 }
 
 try {
-    // This method will be called when Electron has finished
-    // initialization and is ready to create browser windows.
-    // Some APIs can only be used after this event occurs.
-    app.on('ready', () => {
-        createWindow()
+    // Only one instance is allowed
+    if (!app.requestSingleInstanceLock()) {
+        console.warn('Sorry, only one instance is allowed. Terminting...')
+        mainWindow = null
+        app.quit()
+    } else {
+        app.on('second-instance', (event, commandLine, workingDirectory) => {
+            // Someone tried to run a second instance, we should focus our window.
+            if (mainWindow) {
+                if (mainWindow.isMinimized()) mainWindow.restore()
+                mainWindow.focus()
+            }
+        })
 
-        const menuTemplate: Electron.MenuItemConstructorOptions[] = [
-            {
-                label: app.getName(),
-                submenu: [
-                    {
-                        label: 'About',
-                        click() {
-                            win.webContents.send('openAboutModal', '')
-                        },
-                    },
-                    { type: 'separator' },
-                    { role: 'quit' },
-                ],
-            },
-            {
-                label: 'Edit',
-                submenu: [
-                    { role: 'undo' },
-                    { role: 'redo' },
-                    { type: 'separator' },
-                    { role: 'cut' },
-                    { role: 'copy' },
-                    { role: 'paste' },
-                    { role: 'selectAll' },
-                ],
-            },
-        ]
-
-        const menu = Menu.buildFromTemplate(menuTemplate)
-        Menu.setApplicationMenu(menu)
-    })
-
-    // Quit when all windows are closed.
-    app.on('window-all-closed', () => {
-        // On OS X it is common for applications and their menu bar
-        // to stay active until the user quits explicitly with Cmd + Q
-        if (process.platform !== 'darwin') {
-            app.quit()
-        }
-    })
-
-    app.on('activate', () => {
-        // On OS X it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
-        if (win === null) {
+        // This method will be called when Electron has finished
+        // initialization and is ready to create browser windows.
+        // Some APIs can only be used after this event occurs.
+        app.on('ready', () => {
             createWindow()
-        }
-    })
+
+            const menuTemplate: Electron.MenuItemConstructorOptions[] = [
+                {
+                    label: app.getName(),
+                    submenu: [
+                        {
+                            label: 'About',
+                            click() {
+                                mainWindow.webContents.send('openAboutModal', '')
+                            },
+                        },
+                        { type: 'separator' },
+                        { role: 'quit' },
+                    ],
+                },
+                {
+                    label: 'File',
+                    submenu: [{ label: 'Toggle Dev Tools', role: 'toggleDevTools' }],
+                },
+                {
+                    label: 'Edit',
+                    submenu: [
+                        {
+                            label: 'Undo',
+                            accelerator: 'CmdOrCtrl+Z',
+                            role: 'undo',
+                        },
+                        {
+                            label: 'Redo',
+                            accelerator: 'Shift+CmdOrCtrl+Z',
+                            role: 'redo',
+                        },
+                        {
+                            type: 'separator',
+                        },
+                        {
+                            label: 'Cut',
+                            accelerator: 'CmdOrCtrl+X',
+                            role: 'cut',
+                        },
+                        {
+                            label: 'Copy',
+                            accelerator: 'CmdOrCtrl+C',
+                            role: 'copy',
+                        },
+                        {
+                            label: 'Paste',
+                            accelerator: 'CmdOrCtrl+V',
+                            role: 'paste',
+                        },
+                        {
+                            label: 'Select All',
+                            accelerator: 'CmdOrCtrl+A',
+                            role: 'selectAll',
+                        },
+                    ],
+                },
+            ]
+
+            const menu = Menu.buildFromTemplate(menuTemplate)
+            Menu.setApplicationMenu(menu)
+        })
+
+        // Quit when all windows are closed.
+        app.on('window-all-closed', () => {
+            // On OS X it is common for applications and their menu bar
+            // to stay active until the user quits explicitly with Cmd + Q
+            if (process.platform !== 'darwin') {
+                app.quit()
+            }
+        })
+
+        app.on('activate', () => {
+            // On OS X it's common to re-create a window in the app when the
+            // dock icon is clicked and there are no other windows open.
+            if (mainWindow === null) {
+                createWindow()
+            }
+        })
+    }
 } catch (e) {
     // Catch Error
     // throw e;
