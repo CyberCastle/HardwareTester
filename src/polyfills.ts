87a9a236c24e253e4ddb8f4fd91d915b120490f0
CyberCastle <cybercastle@gmail.com>
2019-11-19 20:43:39 -0300

    Update dependencies.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -55,10 +55,15 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone' // Included with Angular CLI.
+import "zone.js/dist/zone"; // Included with Angular CLI.
 //import 'zone.js/dist/zone-mix' // add zone-mix to patch both Browser and Nodejs
 //import 'zone.js/dist/zone-patch-electron' // add zone-patch-electron to patch Electron native API
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+// @ts-ignore
+window.__importDefault = function(mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+};
